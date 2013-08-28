5849d06304dc3fbe89eb32e38467b2fc50f47286
gowrinaidur <gowrinaidur@gmail.com>
2013-08-28 08:56:34 +0530

    Appled current user in sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,8 +6,10 @@ class Users::SessionsController < Devise::SessionsController
 
 	def create
 		if current_user
+			flash[:notice] = "Login Success!!!!!!!!!"
 			redirect_to root_path
 		else
+			flash[:notice] = "Please check your credentials OR Signup here"
 			redirect_to new_user_registration_url
 		end
 	end
